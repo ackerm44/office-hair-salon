e2415a2161b9ec44f1b2f24a32ee83ab2a4fbe25
ackerm44 <ackerm44@gmail.com>
2020-12-18 08:00:17 -0500

    Page routing functionality

--- js/theme.js
+++ js/theme.js
@@ -1,5 +1,32 @@
 $(function() {
     let year = new Date().getFullYear();
-    $('footer .date').html(year);
-
+    $('footer .year').html(year);
+    
+    routing.init();
 });
+
+const routing = {
+    init: function() {
+        hash = window.location.hash;
+        this.getPageData(hash);
+        this.bindMenu();
+    },
+    getPageData: function(hash) {
+        let page = "pages/"+hash.substr(1)+".html"; 
+        if (hash != "") {
+            $.get(page, function(data) {
+                $('main').replaceWith(data);
+            });
+            //also update the url
+            mobileMenu.toggleMenu();
+        }
+    },
+    bindMenu: function() {
+        $('nav p').click(function() {
+            routing.getPageData($(this).data('link'));
+        })
+    },
+    updatePage: function() {
+
+    }
+}
